bc6321a1e44d4fa0c385b363fed70893283747a2
TomaszPosluszny <tomkposluszny@gmail.com>
2023-02-22 19:30:15 +0100

    add structure form in AdPlanner.js

--- src/components/AdPlaner.js
+++ src/components/AdPlaner.js
@@ -1,13 +1,48 @@
 import React from 'react';
 
 class AddPlanner extends React.Component {
+	minDate = new Date().toISOString().slice(0, 10);
 	state = {
-		
+		text: '',
+		checked: false,
+		date: this.minDate,
 	};
+
+	handleText = (e) => {
+		this.setState({text: e.target.value})
+	}
+
+	handleDate = (e) => {
+		this.setState({ date: e.target.value });
+	};
+
+	handleCheckbox = (e) => {
+		this.setState({checked: e.target.value})
+	}
+
+	handleClick  = () =>{}
 	render() {
+		let maxDate = this.minDate.slice(0, 4) * 1 + 1;
+		// maxDate = maxDate + '-12-31';
 		return (
 			<div className='list__header'>
-				<div>Dodaj task</div>
+				<input
+					type='text'
+					placeholder='Dodaj zadanie'
+					value={this.state.text}
+					onChange={this.handleText}
+				/>
+				<input type='checkbox' checked={this.state.checked} id='important' onChange={this.handleCheckbox} />
+				<label htmlFor='important'>Piorytet</label>
+				<label htmlFor='date'>Do kiedy zrobić</label>
+				<input
+					type='date'
+					value={this.state.date}
+					min={this.minDate}
+					max={this.maxDate}
+					onChange={this.handleDate}
+				/>
+				<button onChange={this.handleClick}>Dodaj</button>
 			</div>
 		);
 	}
