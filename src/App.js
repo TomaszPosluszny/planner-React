cb4ad3d52ac049aa30e49d6091675661b07f2418
TomaszPosluszny <tomkposluszny@gmail.com>
2023-03-21 20:28:23 +0100

    add React router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import './App.scss'
-import Planer from './pages/Planer'
+import './App.scss';
+import Registration from './pages/Registration';
+import { Router, Route, browserHistory   } from 'react-router-dom';
+import Planer from './pages/Planer';
 import Footer from './components/Footer/Footer';
 import Nav from './components/Nav';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(fas,fab  )
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, fab);
 
 function App() {
 	return (
+		
 		<div>
-			<Nav/>
-			<Planer/>
-			<Footer/>
-		</div>
+				<Nav />
+				<Planer />
+
+				<Footer />
+				
+				
+				{/* </Routes> */}
+				</div>
+	
 	);
 }
 
---
+++ src/pages/Registration.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class Registration extends React.Component {
+	render() {
+		return <div className='registration'>formularz</div>;
+	}
+}
+
+export default Registration;
